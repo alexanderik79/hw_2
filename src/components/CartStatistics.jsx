b59a51256bc7fd898d2d985e921c62e30381745e
alexanderik79 <alexanderik79@gmail.com>
2025-12-25 19:52:21 +0100

    stat icon

--- src/components/CartStatistics.jsx
+++ src/components/CartStatistics.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function CartStatistics({ itemsCount, totalPrice, actionsCount }) {
   return (
     <div>
-      <h1>Statistics</h1>
+      <h1><ion-icon name="stats-chart-outline"></ion-icon></h1>
       <p>Кількість товарів: {itemsCount}</p>
       <p>Загальна сума: {totalPrice} грн</p>
       <p>Кількість дій: {actionsCount}</p>
